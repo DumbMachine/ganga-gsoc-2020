66f020651282f38d3ce04b5ce3fdba768245fa06
dumbmachine <ratin.kumar.2k@gmail.com>
2020-02-05 17:56:29 +0530

    added mongo for tests

--- database/cassandra/run_docker.sh
+++ database/cassandra/run_docker.sh
@@ -1,3 +1,7 @@
 # Run this script to start the benchmark
-docker pull cassandra
+
 docker run --name cassandra -p 127.0.0.1:9042:9042 -e CASSANDRA_CLUSTER_NAME=GangaTest -e CASSANDRA_ENDPOINT_SNITCH=GossipingPropertyFileSnitch -e CASSANDRA_DC=datacenter1 -d cassandra
+
+docker run --name postgres -p 45432:5432 -e POSTGRES_PASSWORD=ganga -d postgres
+
+docker run --name mongo -p 27017:27017 mongo -d
--- psql.py
+++
@@ -1,4 +0,0 @@
-$ export 1.13.5=1.13.linuxlinuamd64amd64  # change this as you need
-
-$ wget -O /tmp/go1.13.5.linux-amd64.tar.gz https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz && \
-  sudo tar -C /usr/local -xzf /tmp/go1.13.5.linux-amd64.tar.gz
